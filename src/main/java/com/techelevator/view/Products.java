bb87dc305bfbf357ff283e46091bf497f6db7bab
Bardenb <bri.barden@gmail.com>
2022-06-18 11:04:38 -0400

    created product class

---
+++ src/main/java/com/techelevator/view/Products.java
@@ -0,0 +1,57 @@
+package com.techelevator.view;
+
+public class Products {
+    String slot;
+    String name;
+    double price;
+    String type;
+    int quantity;
+    Products(String slot, String name, double price, String type, int quantity){
+        this.slot= slot;
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.quantity = quantity;
+
+    }
+
+    public String getSlot() {
+        return slot;
+    }
+
+    public void setSlot(String slot) {
+        this.slot = slot;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
